c79a0e880e97fa6904ae092082e745c256d208e2
Alberto Salazar <alberto.salazar@ac-techsolutions.com>
2017-12-04 22:36:16 -0500

    Project structure
    Project structure

---
+++ eureka-test/src/main/java/org/fastdev/eurekatest/EurekaTestApplication.java
@@ -0,0 +1,12 @@
+package org.fastdev.eurekatest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaTestApplication.class, args);
+	}
+}
